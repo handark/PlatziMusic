dfb70e2922b8abb44cbaa8e18facab9bfa37f9be
Jose Luis Orozco <handark@gmail.com>
2017-07-17 14:07:32 -0500

    primer commit

---
+++ src/api-client.js
@@ -0,0 +1,17 @@
+const URL = 'https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&api_key=1cf8a18031c8766fba7cfd24aae35ccb&format=json'
+
+function getArtists() {
+    return fetch(URL)
+        .then(response => response.json())
+        .then(data => data.topartists.artist)
+        .then(artists => artists.map( artist => {
+            return {
+                name: artist.name,
+                image: artist.image[3]['#text'],
+                likes: 200,
+                comments: 140,
+            }
+        } ) )
+} 
+
+export { getArtists }
